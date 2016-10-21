6b945182daafb150646a50f75d4d3dde3389cd6d
zanshi <zanshi@live.se>
2016-10-21 14:43:06 +0200

    Initial commit

---
+++ include/Application.h
@@ -0,0 +1,17 @@
+//
+// Created by niclas on 2016-10-21.
+//
+
+#ifndef TSBK03_PROJECT_APPLICATION_H
+#define TSBK03_PROJECT_APPLICATION_H
+
+
+class Application {
+
+
+
+
+};
+
+
+#endif //TSBK03_PROJECT_APPLICATION_H
---
+++ src/Application.cpp
@@ -0,0 +1,5 @@
+//
+// Created by niclas on 2016-10-21.
+//
+
+#include "../include/Application.h"
